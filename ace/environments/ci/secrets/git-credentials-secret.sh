19965e866ef75bc7b842db0ce59f610f6671d41a
Kok Sing Khong <khongks@gmail.com>
2021-10-11 22:23:20 +1100

    Change git-credential-secret name for ace to avoid clash

--- ace/environments/ci/secrets/git-credentials-secret.sh
+++ ace/environments/ci/secrets/git-credentials-secret.sh
@@ -16,7 +16,7 @@ cat <<EOF > delete-git-credentials-secret.yaml
 apiVersion: v1
 kind: Secret
 metadata:
-  name: git-credentials
+  name: git-credentials-ace
   annotations:
     tekton.dev/git-0: ${GIT_BASEURL}
 type: kubernetes.io/basic-auth
